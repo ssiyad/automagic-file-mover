107b0dadc4adfda8e14ec77fad30786f53894dd8
Sabu Siyad <sabu.siyad.p@gmail.com>
2019-10-30 19:05:44 +0530

    Add Logging

--- moover/__init__.py
+++ moover/__init__.py
@@ -1,5 +1,11 @@
 from pathlib import Path
-
+import logging
 from . import *
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+LOGGER = logging.getLogger(__name__)
 path = str(Path.home()) + "/MooverTestDir"
--- moover/__main__.py
+++ moover/__main__.py
@@ -1,10 +1,28 @@
-from watchdog.events import LoggingEventHandler
+import time
+
+from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
-from moover import path
+from moover import path, LOGGER
+
+
+class _FileCreatedPrint(FileSystemEventHandler):
+    def on_created(self, event):
+        LOGGER.info(event)
+        print(event.src_path, type(event.src_path))
+
 
 if __name__ == "__main__":
-    event_handler = LoggingEventHandler()
+    LOGGER.info("Moover started in {}".format(path))
+    event_handler = _FileCreatedPrint()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
