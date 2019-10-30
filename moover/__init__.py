719428db477963d99caebe3020f85d04c0f545bb
Sabu Siyad <sabu.siyad.p@gmail.com>
2019-10-30 21:54:22 +0530

    Almost functional version

--- moover/__init__.py
+++ moover/__init__.py
@@ -1,11 +1,32 @@
-from pathlib import Path
 import logging
-from . import *
+
+from pathlib import Path
+
+import sys
+import os
+
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
 
+DEFAULT = {"SOURCE_DIR": "Downloads", "DESTINATION_DIR": "Moover"}
+
 LOGGER = logging.getLogger(__name__)
-path = str(Path.home()) + "/MooverTestDir"
+
+if len(sys.argv) != 3:
+    if len(sys.argv) > 3:
+        LOGGER.error("Syntax: moover <source_dir> <dest_dir>")
+        exit(0)
+    elif len(sys.argv) == 2:
+        LOGGER.warn("Setting DESTINATION as {}".format(1))
+        SOURCE = os.path.join(Path.home(), DEFAULT["SOURCE_DIR"])
+        DESTINATION = os.path.join(Path.home(), sys.argv[1])
+    else:
+        LOGGER.warn("Using Default SOURCE:MooverTestDir and DESTINATION:Moover")
+        SOURCE = os.path.join(Path.home(), DEFAULT["SOURCE_DIR"])
+        DESTINATION = os.path.join(Path.home(), DEFAULT["DESTINATION_DIR"])
+else:
+    SOURCE = os.path.join(Path.home(), sys.argv[1])
+    DESTINATION = os.path.join(Path.home(), sys.argv[2])
--- moover/__main__.py
+++ moover/__main__.py
@@ -1,22 +1,65 @@
+import os
+import shutil
 import time
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
-from moover import path, LOGGER
+from moover import SOURCE, DESTINATION, LOGGER
 
 
 class _FileCreatedPrint(FileSystemEventHandler):
     def on_created(self, event):
-        LOGGER.info(event)
-        print(event.src_path, type(event.src_path))
+        if os.path.isdir(event.src_path):
+            LOGGER.info("Directory created: {}".format(event.src_path))
+        else:
+            LOGGER.info("File created: {}".format(event.src_path))
+            extension = os.path.splitext(event.src_path)[-1]
+            ext_sub_dir = os.path.join(DESTINATION, extension.upper()[1:])
+            if not _DirFunctions(ext_sub_dir).check_dir():
+                _DirFunctions(ext_sub_dir).make_dir()
+                LOGGER.info("Created new category: {}".format(ext_sub_dir))
+            shutil.move(event.src_path, ext_sub_dir)
+            LOGGER.info("Moved~ Source: {} | Destination: {}".format(event.src_path, ext_sub_dir))
+
+
+class _DirFunctions(object):
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+
+    def check_dir(self):
+        if os.path.exists(self.dir_path):
+            return True
+        else:
+            return False
+
+    def make_dir(self):
+        try:
+            os.mkdir(self.dir_path)
+        except Exception:
+            raise Exception
 
 
 if __name__ == "__main__":
-    LOGGER.info("Moover started in {}".format(path))
+    if not _DirFunctions(SOURCE).check_dir():
+        _DirFunctions(SOURCE).make_dir()
+        LOGGER.info("CREATED SOURCE DIRECTORY: {}".format(SOURCE))
+    if not _DirFunctions(DESTINATION).check_dir():
+        _DirFunctions(DESTINATION).make_dir()
+        LOGGER.info("CREATED DESTINATION DIRECTORY: {}".format(DESTINATION))
+    LOGGER.info("Moover started in {} -> {}".format(SOURCE, DESTINATION))
+    list_dir = [name for name in os.listdir(DESTINATION)
+                if os.path.isdir(os.path.join(DESTINATION, name))]
+
+    if len(list_dir) < 1:
+        sub_dirs = "No Sub-Directories are present in {}".format(DESTINATION)
+    else:
+        sub_dirs = "Sub-Directories present in {}: {}".format(DESTINATION, " | ".join([name for name in list_dir]))
+
+    LOGGER.info(sub_dirs)
     event_handler = _FileCreatedPrint()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, SOURCE, recursive=True)
     observer.start()
 
     try:
